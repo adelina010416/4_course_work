efff2e26b1eb0a67c2bd3ef503fc3cfa532ee2d5
adelina010416 <120559062+adelina010416@users.noreply.github.com>
2023-03-24 01:29:56 +0400

    initial commit

--- dao/model/user.py
+++ dao/model/user.py
@@ -1,14 +1,19 @@
 from marshmallow import Schema, fields
 
 from setup_db import db
+from dao.model.genre import Genre
 
 
 class User(db.Model):
     __tablename__ = 'user'
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String)
-    password = db.Column(db.String)
+    id = db.Column(db.Integer, primary_key=True, nullable=False)
+    email = db.Column(db.String, nullable=False)
+    password = db.Column(db.String, nullable=False)
     role = db.Column(db.String)
+    name = db.Column(db.String)
+    surname = db.Column(db.String)
+    favorite_genre = db.Column(db.String, db.ForeignKey("genre.name"))
+    genre = db.relationship("Genre")
 
 
 class UserSchema(Schema):
@@ -17,3 +22,6 @@ class UserSchema(Schema):
     username = fields.Str()
     password = fields.Str()
     role = fields.Str()
+    name = fields.Str()
+    surname = fields.Str()
+    favorite_genre = fields.Str()
